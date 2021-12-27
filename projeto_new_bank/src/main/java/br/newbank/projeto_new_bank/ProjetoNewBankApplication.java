9fc1bd7daa50544bdce7e7dbb1d563642c1b18c8
TonySilva <tonysilvaol@gmail.com>
2021-12-27 21:42:19 -0200

    inicio de projeto

---
+++ projeto_new_bank/src/main/java/br/newbank/projeto_new_bank/ProjetoNewBankApplication.java
@@ -0,0 +1,13 @@
+package br.newbank.projeto_new_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoNewBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoNewBankApplication.class, args);
+	}
+
+}
---
+++ projeto_new_bank/src/test/java/br/newbank/projeto_new_bank/ProjetoNewBankApplicationTests.java
@@ -0,0 +1,13 @@
+package br.newbank.projeto_new_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoNewBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
