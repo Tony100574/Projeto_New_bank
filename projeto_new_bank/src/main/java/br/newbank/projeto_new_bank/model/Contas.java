fed6649f4c1b25effb5a0a1ceb36bb7cd290187d
AndreEsposito <andrezinhoesposito33@gmail.com>
2021-12-29 10:06:49 -0300

    Criado a tabela no banco de dados

---
+++ projeto_new_bank/src/main/java/br/newbank/projeto_new_bank/model/Contas.java
@@ -0,0 +1,92 @@
+package br.newbank.projeto_new_bank.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Contas")
+public class Contas {
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    
+    @Column(name = "titular", length = 250, nullable = false)
+    private String titular;
+
+    @Column(name = "email", length = 100, nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "numero", nullable = false)
+    private int numero;
+
+    @Column(name = "agencia")
+    private int agencia;
+
+    @Column(name = "saldo", nullable = false)
+    private double saldo;
+
+    @Column(name = "Tipo De Conta", length = 50, nullable = false)
+    private String tipoDeConta;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public String getTipoDeConta() {
+        return tipoDeConta;
+    }
+
+    public void setTipoDeConta(String tipoDeConta) {
+        this.tipoDeConta = tipoDeConta;
+    }
+
+    
+}
