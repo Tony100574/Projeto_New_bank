09a8f8f549670f1ccb12576259ce4e351122842a
AndreEsposito <andrezinhoesposito33@gmail.com>
2021-12-31 20:05:48 -0300

    Conectando o JSON com o HTML part2

--- projeto_new_bank/src/main/resources/public/conectaFront.js
+++ projeto_new_bank/src/main/resources/public/conectaFront.js
@@ -1,13 +1,18 @@
-fetch('contas.json')
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
-        appendData(data);
-    })
-    .catch(function(err) {
-        console.log('error: ' + err);
-    });
+const getInfoBd = async() => {
+
+
+    try {
+        const dataJson = await fetch('contas.json')
+        const data = await dataJson.json()
+
+        appendData(data)
+
+    } catch (error) {
+        console.log(error + "Erro na requisição do servidor.")
+    }
+
+}
+
 
 function appendData(data) {
     var mainContainer = document.getElementById("myData");
@@ -18,5 +23,29 @@ function appendData(data) {
         div.innerHTML = '<br>' + ('id: ' + data[i].id) + '<br>' + ('titular: ' + data[i].titular) + '<br>' + ('email: ' + data[i].email) + '<br>' + ('numero: ' + data[i].numero) + '<br>' + ('agencia: ' + data[i].agencia) + '<br>' + ('saldo: ' + data[i].saldo) + '<br>' + ('tipoDeConta: ' + data[i].tipoDeConta);
 
         mainContainer.appendChild(div);
+    }
+}
+
+
+const showForId = async() => {
+
+    try {
+        const dataJson = await fetch('contas.json')
+        const data = await dataJson.json()
+
+
+        const idCliente = document.querySelector('[idCliente]').value
+        const newData = Array.from(data)
+
+        // isso é a msm coisa que o div.innerHTML da function acima!
+        const infoCliente = newData.filter(cliente => cliente.id === parseInt(idCliente))[0] //.filter(info => info === 'agencia'))
+        const { titular, agencia, saldo } = infoCliente
+        const info2 = { titular, agencia, saldo }
+        return console.log(info2) // document.querySelector('#myData2').innerHTML = info2
+
+    } catch (error) {
+        console.log(error + "Erro na pesquisa por ID.")
+
+
     }
 }
