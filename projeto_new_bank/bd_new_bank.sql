32ddc6618144b375b6c38d05e977c0ee7ed37005
beatricerocha <beatricerocha.it@gmail.com>
2022-01-06 21:51:25 -0300

    Alterado arquivo BD

--- projeto_new_bank/bd_new_bank.sql
+++ projeto_new_bank/bd_new_bank.sql
@@ -1,8 +1,8 @@
 create database bd_new_bank;
 use bd_new_bank;
-
-insert into contas (id, titular, email, agencia, numero, tipo_de_conta, saldo ) values (null, "Ananda Elias", "ananda.elias@email.com", "001-1", "11111-1", "0 - Conta Corrente", 0.00),
-(null, "Andre Esposito", "andre.esposito@email.com", "002-2", "22222-2", "1 - Conta Poupanca", 0.01),
-(null, "Beatrice Rocha", "beatrice.rocha@email.com", "003-3", "33333-3", "2 - Conta Investimento", 0.02),
-(null, "Carlos Silva", "carlos.silva@email.com", "004-4", "44444-4", "0 - Conta Corrente", 0.03),
-(null, "Antonio Silva", "antonio.silva@email.com", "005-5", "55555-5", "0 - Conta Corrente", 0.10);
+select * from contas;
+insert into contas (id, titular, email, agencia, numero, saldo, tipo_de_conta) values (null, "Ananda Elias", "ananda.elias@email.com", "0001", "11111-1", 100.00, "0 - Conta Corrente"),
+(null, "Andre Esposito", "andre.esposito@email.com", "0001", "22222-2",  150.0, "0 - Conta Corrente"),
+(null, "Beatrice Rocha", "beatrice.rocha@email.com", "0002", "11111-1",100.0, "1 - Conta Poupança"),
+(null, "Carlos Silva", "carlos.silva@email.com", "0002", "22222-2", 250.0, "2 - Conta Investimentos"),
+(null, "Antonio Silva", "antonio.silva@email.com", "0003", "33333-3", 500.0, "2 - Conta Investimentos");
