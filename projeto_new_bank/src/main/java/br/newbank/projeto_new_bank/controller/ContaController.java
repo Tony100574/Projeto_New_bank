3f5ebfd1d8a7b002ea3bdf91580d12df071ceeb7
beatricerocha <beatricerocha.it@gmail.com>
2022-01-06 21:30:25 -0300

    Insert Method PUT and DELETE

--- projeto_new_bank/src/main/java/br/newbank/projeto_new_bank/controller/ContaController.java
+++ projeto_new_bank/src/main/java/br/newbank/projeto_new_bank/controller/ContaController.java
@@ -3,11 +3,14 @@ package br.newbank.projeto_new_bank.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -49,5 +52,29 @@ public class ContaController {
 
         return ResponseEntity.ok(novaConta);
     }
-    
+
+    @PutMapping
+    public ResponseEntity<Contas> atualizarConta(@RequestBody Contas contas) {
+        if (contas.getId() > 0) {
+            Contas atualizarConta = repositorioContas.save(contas);
+
+            return ResponseEntity.ok(atualizarConta);
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> apagarConta(@PathVariable long id) {
+        Contas contaEncontrada = repositorioContas.findById(id).orElse(null);
+        if (contaEncontrada != null) {
+            try {
+                repositorioContas.deleteById(id);
+                return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+
+            } catch (Exception e) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+            }
+        }
+        return ResponseEntity.notFound().build();
+    }
 }
